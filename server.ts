81145ceb0bd381029ffbfe049fd1a5d357df90c8
Tony <tny.alberty@gmail.com>
2025-07-14 21:55:26 -0400

    <-.-<

---
+++ server.ts
@@ -0,0 +1,9 @@
+import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+
+Deno.serve((req) => {
+  return serveDir(req, {
+    fsRoot: "./public",
+    urlRoot: "",
+    showDirListing: true,
+  });
+});
